94da1129e288ffb2de6420ce827ece852d3135d6
barbareshka <119070110+barbareshka@users.noreply.github.com>
2023-01-26 12:02:29 +0300

    Create test_ds2rec.py

---
+++ test/test_ds2rec.py
@@ -0,0 +1,19 @@
+import torch
+from kospeech.models import DeepSpeech2
+
+batch_size = 3
+sequence_length = 14321
+dimension = 80
+
+cuda = torch.cuda.is_available()
+device = torch.device('cuda' if cuda else 'cpu')
+
+inputs = torch.rand(batch_size, sequence_length, dimension).to(device)  # BxTxD
+input_lengths = torch.LongTensor([14321, 14300, 13000]).to(device)
+
+print("Deep Speech 2 Model Test..")
+model = DeepSpeech2(num_classes=10, input_dim=dimension).to(device)
+output = model.recognize(inputs, input_lengths)
+
+print(output)
+print(output.size())
